7b11cef4d5e990a7b2b7ed4ae62c94ef98b8bc7d
frsilence <zgqqzyf@163.com>
2019-02-26 17:24:40 +0800

    4

---
+++ src/test50/Student.java
@@ -0,0 +1,10 @@
+package test50;
+public class Student{
+	private String name;
+    private String studentNumber;
+    private int[] score = new int[3];
+    @Override
+    public String toString() {
+    	return ""
+    }
+}
